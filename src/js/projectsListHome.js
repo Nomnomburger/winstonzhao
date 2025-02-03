7e9c0685cfe7f3a0aa99a54b2926fb3ea3e741cd
Winston Zhao <winstonzhao.ca@outlook.com>
2025-02-02 23:41:23 -0500

    upadtes

--- src/js/projectsListHome.js
+++ src/js/projectsListHome.js
@@ -9,7 +9,7 @@ class ProjectList extends HTMLElement {
       <div class="row p-list-row g-0 aBorder p-list-row-first just-fade">
         <div class="col-7 col-xl-6 col-lg-5 small-bold p-list-title"><a>Heatmap.com Redesign</a><span class="p-list-date">2024</span></div>
         <div class="col-0 col-xl-5 col-lg-6 paragraph p-list-desc">Product Design Intern @ Full Sprint Studio</div>
-        <div class="col-5 col-lg-1 small-bold p-list-btn"><a>COMING SOON</a></div>
+        <div class="col-5 col-lg-1 small-bold p-list-btn"><a href="../portfolio/heatmap.html">READ</a></div>
       </div>
       <div class="row p-list-row g-0 aBorder just-fade">
         <div class="col-7 col-xl-6 col-lg-5 small-bold p-list-title"><a>UHS Interface</a><span class="p-list-date">2023</span></div>
