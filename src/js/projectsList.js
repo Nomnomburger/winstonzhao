489086b0464fc34a982a2cacc9288f15d84390b8
Winston Zhao <winstonzhao.ca@outlook.com>
2025-02-04 12:34:46 -0500

    finish heatmap portfolio item

--- src/js/projectsList.js
+++ src/js/projectsList.js
@@ -6,10 +6,11 @@ class ProjectList extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <div class="row p-list-row g-0 aBorder p-list-row-first just-fade">
-      <div class="col-7 col-xl-6 col-lg-5 small-bold p-list-title"><a>Heatmap.com Redesign</a><span class="p-list-date">2024</span></div>
-      <div class="col-0 col-xl-5 col-lg-6 paragraph p-list-desc">Product Design Intern @ Full Sprint Studio</div>
-      <div class="col-5 col-lg-1 small-bold p-list-btn"><a>COMING SOON</a></div>
+      <div class="col-7 col-xl-6 col-lg-5 small-bold p-list-title"><a href="../portfolio/heatmap.html">Heatmap Portal Redesign</a><span class="p-list-date">2024</span></div>
+      <div class="col-0 col-xl-5 col-lg-6 paragraph p-list-desc">Product Design Intern @ Full Sprint</div>
+      <div class="col-5 col-lg-1 small-bold p-list-btn"><a href="../portfolio/heatmap.html">READ</a></div>
     </div>
+    <!--
     <div class="row p-list-row g-0 aBorder just-fade">
       <div class="col-7 col-xl-6 col-lg-5 small-bold p-list-title"><a>UHS Interface</a><span class="p-list-date">2023</span></div>
       <div class="col-0 col-xl-5 col-lg-6 paragraph p-list-desc">UI/UX design club at Unionville High School</div>
@@ -20,6 +21,7 @@ class ProjectList extends HTMLElement {
       <div class="col-0 col-xl-5 col-lg-6 paragraph p-list-desc">Branding and media for the UHS Robotics Team</div>
       <div class="col-5 col-lg-1 small-bold p-list-btn"><a>COMING SOON</a></div>
     </div>
+    -->
     <div class="row p-list-row g-0 aBorder just-fade">
       <div class="col-10 col-xl-6 col-lg-5 small-bold p-list-title"><a href="../portfolio/yrconnect.html">YRConnect</a><span class="p-list-date">2024</span></div>
       <div class="col-0 col-xl-5 col-lg-6 paragraph p-list-desc"> An app that centralizes access to city services for York Region residents.</div>
